de48bab34c21c89e3a0a0c8b53876b40e5e940e4
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-05-13 22:39:45 -0400

    6kyu parity outlier, needs finishing

---
+++ ParityOutlier/main.js
@@ -0,0 +1,47 @@
+// You are given an array (which will have a length of at least 3, but could be very large)
+// containing integers. The array is either entirely comprised of odd integers or entirely
+// comprised of even integers except for a single integer N. Write a method that takes the array
+// as an argument and returns this "outlier" N.
+
+//PREP - parameters, returns, examples, psuedocode
+
+// parameters - an array of at least length 3 -> very large (containing integers)
+// this array is either all odd or all even expect for a single integer
+
+// returns -> a single integer
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+// psuedocode
+// check first middle and last value of array, at least two will match even or odd
+// filter array by the opposite value, leaving one value left over
+
+const parityOutlierFinder = (array) => {
+  let even = 0;
+  let odd = 0;
+
+  [array[0], array[1], array[2]].forEach((element) =>
+    array % 2 === 0 ? even++ : odd++
+  );
+
+  array.filter((element) => {
+    if (even > odd) {
+      return element % 2 === 0;
+    } else {
+      return element % 2 > 0;
+    }
+  });
+
+  return array[0];
+};
+
+console.log(parityOutlierFinder([2, 4, 0, 100, 4, 11, 2602, 36]));
+// returns 11
+
+console.log(parityOutlierFinder([160, 3, 1719, 19, 11, 13, -21]));
+// returns 160
