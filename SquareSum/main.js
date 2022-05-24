e43cbdc0d3cc97b08776c3e68c174858496c8850
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-05-24 16:34:12 -0400

    8kyu daily, finished yesterday

---
+++ SquareSum/main.js
@@ -0,0 +1,27 @@
+//PREP
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+//parameter - an array of integers, can possibly be empty array
+
+//returns - a single integer value, if empty array returns 0
+
+//examples
+// squareSum([1,2,2]) => 9
+// squareSum([0,3,4,5]) => 50
+
+//psuedocode
+// iterate over integers
+//   map the squares of each to a new array
+// return sum of the new array
+const squareSum = (numbers) => {
+  return numbers
+    .map((element) => element * element)
+    .reduce((acc, element) => acc + element, 0);
+};
+
+testArray = [1, 2, 2];
+testArrayTwo = [0, 3, 4, 5];
+console.log(squareSum(testArray));
+console.log(squareSum(testArrayTwo));
