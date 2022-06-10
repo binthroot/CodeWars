30908150feee1224de6a3b260dff2257bd204613
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-06-09 23:39:45 -0400

    finish camelCase

--- toCamelCase/main.js
+++ toCamelCase/main.js
@@ -1,12 +1,19 @@
 const toCamelCase = (str) => {
-  return str
-    .replace(/-|_/g, ' ')
-    .split('')
-    .map((char, index, array) => {
-      if (array[index - 1] === ' ') return char.toUpperCase();
-      return char;
-    })
-    .join('');
+  return (
+    str
+      .replace(/-|_/g, ' ')
+      .split(' ')
+      // .map((char, index, array) => {
+      //   if (array[index - 1] === ' ') return char.toUpperCase();
+      //   return char;
+      // })
+      .map((word, index) => {
+        if (index !== 0) return word[0].toUpperCase() + word.slice(1);
+        return word;
+      })
+      .join('')
+      .replace(' ', '')
+  );
 };
 
 test = 'the-one-you';
@@ -14,4 +21,3 @@ testTwo = 'the_one_you';
 
 console.log(toCamelCase(test));
 console.log(toCamelCase(testTwo));
-console.log('');
