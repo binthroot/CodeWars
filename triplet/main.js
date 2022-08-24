dae4ab440872cb6f22f91ec84a2ea793c80a7966
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-08-24 18:56:35 -0400

    prep of codewars triplets

---
+++ triplet/main.js
@@ -0,0 +1,11 @@
+// param - an array of three numbers
+
+// returns - the index of the number that is consecutively the middle of the three numbers 
+
+// example func([1, 8, 4]) => 2 (the index of 4 in the param array)
+
+//  psuedo
+//  Func(array of three nums) =>
+//    Set new array = copt of param array
+//    Sort the new array highest to lowest
+//    Return paramArray.indexOf(newArray[1])
