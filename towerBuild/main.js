6b1ff56a1b76623825bb6570892d87412eeb87ff
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-08-27 21:52:12 -0400

    6kyu tower build v2

--- towerBuild/main.js
+++ towerBuild/main.js
@@ -1,8 +1,8 @@
 const towerBuild = (numOfFloors) => {
 	towerArray = [];
-	for (let i = 0; i <= numOfFloors; i++) {
+	for (let i = 1; i <= numOfFloors; i++) {
 		let space = ' '.repeat(numOfFloors - i);
-		let star = '*'.repeat((2i) - 1);
+		let star = '*'.repeat((2*i) - 1);
 		towerArray.push(space + star + space);
 	}
 	return towerArray;
