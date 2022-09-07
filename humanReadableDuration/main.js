d3d0feaede97a1203543ea5bf7e3e13094bb4bec
Ben Thrasher <ben.r.thrasher@gmail.com>
2022-09-07 13:07:20 -0400

    initial work code wars 4kyu human readable duration

---
+++ humanReadableDuration/main.js
@@ -0,0 +1,14 @@
+function formatDuration(seconds) {
+  const year = 31557600;
+  const day = 86400;
+  const hour = 3600;
+  const minute = 60;
+  let secondsLeft = seconds;
+  let resultString = '';
+
+  if (secondLefts > year) {
+    let yearString = Math.floor(secondsLeft / year);
+  }
+}
+
+// initial work for human readable duration code wars 4kyu
